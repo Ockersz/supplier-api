3634119d35f427e8651fe0c39fd3ac25d3acfc72
Shahein Ockersz <shaheinockersz1234@gmail.com>
2025-07-03 13:16:41 +0530

    feat: add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
   });
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Supplier API')
+    .setDescription('The supplier API description')
+    .setVersion('1.0')
+    .addTag('suppliers')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
